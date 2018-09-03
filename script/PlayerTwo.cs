ee38d235496f8be49d4cbb76593e7559f74653bd
ahilmanp <ahilman.prasetya@gmail.com>
2018-09-03 18:06:34 +1000

    Update PlayerTwo.cs

--- script/PlayerTwo.cs
+++ script/PlayerTwo.cs
@@ -1,22 +1,41 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(PlayerControllerTwo))]
 public class PlayerTwo : MonoBehaviour
 {
 
-    public float moveSpeed = 7f;
-    PlayerControllerTwo controller;
+    public float moveSpd = 5f;
+    PlayerControllerTwo cont;
 
     void Start()
     {
-        controller = GetComponent<PlayerControllerTwo>();
+        cont = GetComponent<PlayerControllerTwo>();
     }
 
     void Update()
     {
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("HorizontalTwo"), 0, Input.GetAxisRaw("VerticalTwo"));
-        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
-        controller.Move(moveVelocity);
+        Vector3 moveVelocity = moveInput.normalized * moveSpd;
+        cont.Move(moveVelocity);
+    }
+
+    public void SpeedUp(string Player)
+    {
+        if (Player == "Player2")
+        {
+            moveSpd += 5f;
+        }
+    }
+
+    public void PositionItem(string Player)
+    {
+        if (Player == "Player2")
+        {
+            //var MyGameObjectPosition = GameObject.Find("Player1").transform.position;
+            //MyGameObjectPosition = new Vector3 (-8, 1, 8);
+            //print(MyGameObjectPosition);
+            GameObject.Find("Player1").transform.position = new Vector3(-8.45f, 0.5f, 8.67f);
+        }
     }
 }
